bd389748967caa97bcc96a65396ae0fe9999459d
bensearle <bensearle93@hotmail.co.uk>
2016-05-20 20:36:57 +0800

    End of day

--- Mapper_3/app/src/main/java/bensearle/mapper_3/Algorithms.java
+++ Mapper_3/app/src/main/java/bensearle/mapper_3/Algorithms.java
@@ -1,6 +1,8 @@
 package bensearle.mapper_3;
 
+import bensearle.mapper_3.Structures.Circle3D;
 import bensearle.mapper_3.Structures.Fingerprint;
+import bensearle.mapper_3.Structures.Point3D;
 
 /**
  * Created by bensearle on 19/05/2016.
@@ -11,20 +13,32 @@ public class Algorithms {
      * the distance between an unknown point and a point of reference
      * @return the distance
      */
-    public double Euclidean_Distance(Fingerprint f1, Fingerprint f2){
+    public double Euclidean_Distance(int[] values1, int[] values2){
         double d;
 
         double Sum = 0.0;
 
-        for(int i=0;i<array1.length;i++) {
-            Sum = Sum + Math.pow((array1[i]-array2[i]),2.0);
+        for(int i=0;i<values1.length;i++) {
+            Sum = Sum + Math.pow((values1[i]-values2[i]),2.0);
         }
 
-        return Math.sqrt(Sum);
+        d = Math.sqrt(Sum);
 
         return d;
     }
 
+
+    public void CircleIntercetion(Circle3D circle1, Circle3D circle2){
+
+    }
+
+    public Point3D TriangleCentre (Point3D p1, Point3D p2, Point3D p3){
+        Point3D center12 = new Point3D((p1.X+p2.X)/2, (p1.Y+p2.Y)/2, (p1.Z+p2.Z)/2);
+        Point3D center13 = new Point3D((p1.X+p3.X)/2, (p1.Y+p3.Y)/2, (p1.Z+p3.Z)/2);
+        Point3D center23 = new Point3D((p2.X+p3.X)/2, (p2.Y+p3.Y)/2, (p2.Z+p3.Z)/2);
+
+
+    }
 
 
 }
