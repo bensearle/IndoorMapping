4e65479029445dcba510256ebadf13ff664d921b
bensearle <bensearle93@hotmail.co.uk>
2016-05-20 20:37:13 +0800

    End of day

--- Mapper_3/app/src/main/java/bensearle/mapper_3/Structures/Point3D.java
+++ Mapper_3/app/src/main/java/bensearle/mapper_3/Structures/Point3D.java
@@ -7,4 +7,10 @@ public class Point3D {
     public double X;
     public double Y;
     public double Z;
+
+    public Point3D (double x, double y, double z){
+        X=x;
+        Y=y;
+        Z=z;
+    }
 }
