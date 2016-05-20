096c28426526d1e466951a476151b26049548d57
bensearle <bensearle93@hotmail.co.uk>
2016-05-20 17:35:46 +0800

    3D Point structure

---
+++ Mapper_3/app/src/main/java/bensearle/mapper_3/Structures/Point3D.java
@@ -0,0 +1,10 @@
+package bensearle.mapper_3.Structures;
+
+/**
+ * Created by bensearle on 19/05/2016.
+ */
+public class Point3D {
+    public double X;
+    public double Y;
+    public double Z;
+}
